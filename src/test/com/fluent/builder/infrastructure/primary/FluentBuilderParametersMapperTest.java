b025d3ec3994ce6605dc522ac17b3cdb19be31cd
fabienpuissant <fabienpuissant@live.fr>
2024-11-22 23:10:58 +0100

    gradle 2

--- src/test/com/fluent/builder/infrastructure/primary/FluentBuilderParametersMapperTest.java
+++ src/test/com/fluent/builder/infrastructure/primary/FluentBuilderParametersMapperTest.java
@@ -1,16 +1,10 @@
 package com.fluent.builder.infrastructure.primary;
 
 
-import com.intellij.ide.highlighter.JavaFileType;
+
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiFileFactory;
-import com.intellij.psi.PsiJavaFile;
-import com.intellij.psi.impl.source.PsiClassImpl;
-import com.intellij.psi.util.PsiUtil;
-import com.intellij.testFramework.LightPlatformTestCase;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -18,7 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class FluentBuilderParametersMapperTest extends LightPlatformTestCase {
+public class FluentBuilderParametersMapperTest  {
 
     private Project project = mock(Project.class);
 
